7711173224bc24a10008da7ce50804729450411d
guidaohuangxing <guidao12@163.com>
2021-09-27 16:20:31 -0400

    Scene

--- Assets/Scripts/Jennifer/ChimesGroup.cs
+++ Assets/Scripts/Jennifer/ChimesGroup.cs
@@ -77,7 +77,7 @@ public class ChimesGroup : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //print(other.gameObject.name);
-        if (other.gameObject.CompareTag("MainCamera") && !startThePuzzle)
+        if (other.gameObject.CompareTag("Boat") && !startThePuzzle)
         {
             startThePuzzle = true;
             WhenPlayerEntered();
