70ae61e95605eed31f2fc91f566427a1ee806512
2015301140020@whu.edu.cn <Luna0minos4>
2021-10-01 21:04:54 -0400

    Bloom, 花灯飞在WaterTest.unity

--- Assets/Scripts/Jennifer/EndScene.cs
+++ Assets/Scripts/Jennifer/EndScene.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class EndScene : MonoBehaviour
 {
+    private GameObject EndSceneLotus;
+    private float speed = 0.2f;
+    private float spreadEle = 2f;
+    //private GameObject[] EndLotus;
     //flower
+    private void Start()
+    {
+        EndSceneLotus = GameObject.Find("EndSceneLotus");
+
+    }
+    private void Update()
+    {
+        foreach (var i in EndSceneLotus.GetComponentsInChildren<EndSceneLotus>())
+        {
+            i.transform.position = Vector3.MoveTowards(i.transform.position, new Vector3(i.transform.position.x * spreadEle, 100, i.transform.position.z), Time.deltaTime * speed * Mathf.Abs(i.transform.position.x));
+
+        }
+    }
+
+
+
 }
